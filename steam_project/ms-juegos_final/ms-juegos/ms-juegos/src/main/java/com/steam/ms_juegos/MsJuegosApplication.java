5584878a99a76b84a39c821123fccac542df2915
Luca-1709 <lucascale1709@gmail.com>
2026-06-22 00:40:33 -0400

    Juegos Listo

--- steam_project/ms-juegos_final/ms-juegos/ms-juegos/src/main/java/com/steam/ms_juegos/MsJuegosApplication.java
+++ steam_project/ms-juegos_final/ms-juegos/ms-juegos/src/main/java/com/steam/ms_juegos/MsJuegosApplication.java
@@ -2,9 +2,11 @@ package com.steam.ms_juegos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 @EnableDiscoveryClient
 public class MsJuegosApplication {
 
--- steam_project/ms-usuario_final/ms-usuario_terminado/ms-usuarios/ms-usuarios/src/main/java/com/steam/ms_usuarios/config/SwaggerConfig.java
+++ steam_project/ms-usuario_final/ms-usuario_terminado/ms-usuarios/ms-usuarios/src/main/java/com/steam/ms_usuarios/config/SwaggerConfig.java
@@ -1,22 +1,12 @@
-package main.java.com.steam.ms_usuarios.config;
+package com.steam.ms_usuarios.config;
 
 
 import org.springframework.context.annotation.Configuration;
-import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Info;
 import org.springframework.context.annotation.Bean;
+
 
 @Configuration
 
 public class SwaggerConfig {
-
-    @Bean
-    public OpenAPI customOpenAPI() {
-        return new OpenAPI()
-                .info(new Info()
-                        .title("MS-Usuarios API")
-                        .version("1.0.0")
-                        .description("API para gestión de usuarios"));
-    }
 
 }
